dc7d43a4a270015e5afa3f14d7d464bee4f9496d
Rahul Wadhwani <rahulwadhwani21@gmail.com>
2020-04-22 11:36:21 +0100

    Questions solved on 20200421

---
+++ src/main/java/leetcode/Array/AssignCookies.java
@@ -0,0 +1,26 @@
+package main.java.leetcode.Array;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+    /**
+     * Problem: https://leetcode.com/problems/assign-cookies/
+     * @param g
+     * @param s
+     * @return
+     */
+    public int findContentChildren(int[] g, int[] s) {
+        int contentChildren = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int cookieCounter = s.length - 1;
+        for(int i = g.length - 1; i >= 0; i--) {
+            if(s[cookieCounter] >= g[i]) {
+                cookieCounter--;
+                contentChildren++;
+            }
+            if(cookieCounter < 0) break;
+        }
+        return contentChildren;
+    }
+}
