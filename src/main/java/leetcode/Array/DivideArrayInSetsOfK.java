780162b46b18958bbd7fc090a43c1f979a04db03
Rahul Wadhwani <rahulwadhwani21@gmail.com>
2020-06-24 10:57:35 +0100

    Snapshot Array, DivideArrayInSetsOfK, WordLadder question

--- src/main/java/leetcode/Array/DivideArrayInSetsOfK.java
+++ src/main/java/leetcode/Array/DivideArrayInSetsOfK.java
@@ -1,4 +1,41 @@
 package main.java.leetcode.Array;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
+// Input: nums = [1,2,3,3,4,4,5,6], k = 4
+// Output: true
+// Explanation: Array can be divided into [1,2,3,4] and [3,4,5,6].
+
 public class DivideArrayInSetsOfK {
+  public boolean isPossibleDivide(int[] nums, int k) {
+    HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+    for (int num : nums) {
+      frequencyMap.putIfAbsent(num, 0);
+      frequencyMap.put(num, frequencyMap.get(num) + 1);
+    }
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length; i++) {
+      if (frequencyMap.get(nums[i]) >= 1) {
+        for (int j = 0; j < k; j++) {
+          int currentElement = nums[i] + j;
+          if (frequencyMap.containsKey(currentElement) && frequencyMap.get(currentElement) <= 0) {
+            return false;
+          } else {
+            frequencyMap.put(currentElement, frequencyMap.get(currentElement) - 1);
+          }
+        }
+      }
+    }
+    for (Integer value : frequencyMap.values()) {
+      if (value > 0) return false;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {1, 2, 3, 3, 4, 4, 5, 6};
+    DivideArrayInSetsOfK d = new DivideArrayInSetsOfK();
+    System.out.println(d.isPossibleDivide(nums, 4));
+  }
 }
--- src/main/java/leetcode/graphs/WordLadder.java
+++ src/main/java/leetcode/graphs/WordLadder.java
@@ -1,4 +1,60 @@
 package main.java.leetcode.graphs;
 
+import java.util.*;
+
 public class WordLadder {
+  public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    Set<String> seen = new HashSet<>();
+    LinkedList<String> queue = new LinkedList<>();
+    queue.add(beginWord);
+    int result = 0;
+    while (!queue.isEmpty()) {
+      ++result;
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        String currentWord = queue.poll();
+        if (currentWord.equals(endWord)) return result;
+        List<String> children = getChildren(currentWord, wordList);
+        for (String child : children) {
+          if (!seen.contains(child)) {
+            queue.offer(child);
+            seen.add(child);
+          }
+        }
+      }
+    }
+    return 0;
+  }
+
+  private List<String> getChildren(String word, List<String> wordList) {
+    List<String> result = new ArrayList<>();
+    for (String str : wordList) {
+      int count = 0;
+      for (int i = 0; i < word.length(); i++) {
+        if (str.charAt(i) != word.charAt(i)) {
+          count++;
+        }
+      }
+      if (count == 1) {
+        result.add(str);
+      }
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    WordLadder wl = new WordLadder();
+    String[] words = {
+      "si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av",
+      "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr",
+      "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as",
+      "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz",
+      "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st",
+      "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr",
+      "pa", "he", "lr", "sq", "ye"
+    };
+    ArrayList<String> a = new ArrayList<>(Arrays.asList(words));
+
+    wl.ladderLength("qa", "sq", a);
+  }
 }
