59962ba771b5949a798730d6d70dc4a29ab16ee7
Rahul Wadhwani <rahulwadhwani21@gmail.com>
2020-08-02 20:59:34 +0100

    Solved KthGreatestElementsInAMaxHeap question

--- src/main/java/leetcode/PriorityQueue/KthGreatestElementsInAMaxHeap.java
+++ src/main/java/leetcode/PriorityQueue/KthGreatestElementsInAMaxHeap.java
@@ -2,15 +2,16 @@ package main.java.leetcode.PriorityQueue;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class KthGreatestElementsInAMaxHeap {
 
-  private int getLeftChild(int index) {
+  private int getLeftChildIndex(int index) {
     return index * 2 + 1;
   }
 
-  private int getRightChild(int index) {
+  private int getRightChildIndex(int index) {
     return index * 2 + 2;
   }
 
@@ -27,17 +28,30 @@ public class KthGreatestElementsInAMaxHeap {
   private class NodeComparator implements Comparator<Node> {
     @Override
     public int compare(Node n1, Node n2) {
-      return Integer.compare(n1.num, n2.num);
+      return Integer.compare(n2.num, n1.num);
     }
   }
 
   public List<Integer> kLargestInImmutableHeap(int[] immutableHeap, int k) {
-    if (k == 0) {
+    if (k <= 0) {
       return new ArrayList<Integer>();
     }
     int initialCapacity = k + 1;
+    List<Integer> kthLargest = new ArrayList<>();
     PriorityQueue<Node> queue = new PriorityQueue<>(initialCapacity, new NodeComparator());
     queue.add(new Node(0, immutableHeap[0]));
-    for (int i = 0; i < k; i++) {}
+    for (int i = 0; i < k; i++) {
+      Node currentNode = queue.poll();
+      kthLargest.add(currentNode.num);
+      int leftChildIndex = getLeftChildIndex(currentNode.index);
+      if (leftChildIndex < immutableHeap.length) {
+        queue.add(new Node(leftChildIndex, immutableHeap[leftChildIndex]));
+      }
+      int rightChildIndex = getRightChildIndex(currentNode.index);
+      if (rightChildIndex < immutableHeap.length) {
+        queue.add(new Node(rightChildIndex, immutableHeap[rightChildIndex]));
+      }
+    }
+    return kthLargest;
   }
 }
